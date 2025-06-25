d243c60820f87689fef69deaa0cc21264679460f
Muzammil <muzammilarbi00035@gmail.com>
2025-06-25 17:37:48 +0500

    N/A : Update in script

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const { App } = require('@slack/bolt');
+const { App, ExpressReceiver } = require('@slack/bolt');
 require('dotenv').config();
+
+
+const receiver = new ExpressReceiver({
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
 
 const onboardingFlow = require('./flows/onboardingFlow');
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    receiver
 });
 
 onboardingFlow(app);
+receiver.router.get('/', (req, res) => {
+    res.send('✅ Moha Slack Bot is running');
+});
+
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Moha Slack bot is running on HTTP Events API!');
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Moha Slack bot is running on HTTP Events API!');
 })();
